afe5a2c3bed6b8c55be0ee167d8546a9841f6909
shearaell <kosiorekktrz@gmail.com>
2019-12-15 19:09:52 +0100

    25. Simplifying With Functions

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -4,39 +4,50 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessPrintBack();
+
+
+
+//the entry point for our application
 int main()
 {
-	//introduce the game
-	constexpr int WORD_LENGHT = 5;
-	cout <<"Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORD_LENGHT;
-	cout <<	" letter isogram I'm thinking of?\n";
-	cout << endl;
+	PrintIntro();
+	
+	
+	
+	GetGuessPrintBack();
 
-	//get the guess from the player
-	cout << "Enter your guess: ";
-	string Guess = "";
-	getline(cin,Guess);
+	GetGuessPrintBack();
 
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess;
-
-	//get the guess from the player
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess;
-
-	//get the guess from the player
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess;
+	GetGuessPrintBack();
 	
 	cout << endl;
 
 
 	return	0;
+}
+
+void PrintIntro()
+{
+	//introduce the game
+	constexpr int WORD_LENGHT = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGHT;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+string GetGuessPrintBack() 
+{
+	//get the guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess;
+
+	cout << endl;
+	return Guess;
 }
